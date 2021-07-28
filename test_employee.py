397f65fd60ab77e1b3099b9ff6e468548ebc6595
Tung Tran <tung.tran@pap-tech.com>
2021-07-28 08:16:45 +0700

    update test_employee

--- test_employee.py
+++ test_employee.py
@@ -15,7 +15,7 @@ class TestEmployee(unittest.TestCase):
 
     def setUp(self):
         print('setUp')
-        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_1 = Employee('Tung', 'Tran', 50000)
         self.emp_2 = Employee('Sue', 'Smith', 60000)
 
     def tearDown(self):
@@ -23,24 +23,24 @@ class TestEmployee(unittest.TestCase):
 
     def test_email(self):
         print('test_email')
-        self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
+        self.assertEqual(self.emp_1.email, 'Tung.Tran@email.com')
         self.assertEqual(self.emp_2.email, 'Sue.Smith@email.com')
 
         self.emp_1.first = 'John'
         self.emp_2.first = 'Jane'
 
-        self.assertEqual(self.emp_1.email, 'John.Schafer@email.com')
+        self.assertEqual(self.emp_1.email, 'John.Tran@email.com')
         self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
 
     def test_fullname(self):
         print('test_fullname')
-        self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
+        self.assertEqual(self.emp_1.fullname, 'Tung Tran')
         self.assertEqual(self.emp_2.fullname, 'Sue Smith')
 
         self.emp_1.first = 'John'
         self.emp_2.first = 'Jane'
 
-        self.assertEqual(self.emp_1.fullname, 'John Schafer')
+        self.assertEqual(self.emp_1.fullname, 'John Tran')
         self.assertEqual(self.emp_2.fullname, 'Jane Smith')
 
     def test_apply_raise(self):
